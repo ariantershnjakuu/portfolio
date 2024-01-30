b0b6ac0034824532f8530b829a3f18b0fde8c3ab
Arian Tershnjaku <arian.t@gjirafa.com>
2024-01-31 00:20:36 +0100

    Initial Commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+interface NavbarProps {}
+
+const Navbar: React.FC<NavbarProps> = () => {
+  return (
+    <div className="flex items-center justify-between">
+      <h1>Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
